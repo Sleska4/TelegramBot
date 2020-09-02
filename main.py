078ae6ab8fd8158f1beb3f1478a8aac1b79d1cbc
Aleksey <Shumakov4l@yandex.ru>
2020-09-03 00:07:15 +0300

    Revert "Добавленна проверка коректности введеного времени. Удалены лишние библиотеки. Начал работать над кнопкой удалить и экстренным сохранением данных."
    This reverts commit 34cd353c50bec55c780f3827dec552eb0963e547.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import config
 import telebot
 import random
+from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -13,9 +14,9 @@ def welcome(message):
     len(config.hello_text) - 1)].format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=config.menu())
 
 
-def add_f(message):
+def addF(message):
     add = message.text
-    config.arr.append(['', add])
+    config.arr.append([add])
     print(config.arr)
     bot.send_message(message.chat.id, 'Текст успешно добавлен')
     time = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -28,41 +29,11 @@ def add_f(message):
 def time_add(message):
     time = message.text
     if time != "Нет, спасибо ❌":
-        try:
-            time_boolean = False
-            time = str(time)
-            time = time.split(':')
-            if 0 < int(time[0]) < 24:
-                if 0 < int(time[1]) < 60:
-                    if 0 < int(time[2]) < 60:
-                        time_boolean = True
-            if time_boolean:
-                time = ':'.join(time)
-                config.arr[len(config.arr) - 1].append(time)
-                bot.send_message(message.chat.id, 'Данные успешно обновлены ✅', reply_markup=config.menu())
-            else:
-                bot.send_message(message.chat.id, 'Вы ввели какую-то поебень 😱', reply_markup=config.menu())
-                del config.arr[-1]
-        except:
-            bot.send_message(message.chat.id, 'Вы ввели какую-то поебень 😱', reply_markup=config.menu())
-            del config.arr[-1]
+        config.arr[len(config.arr) - 1].append(time)
+        bot.send_message(message.chat.id, 'Данные успешно обновлены', reply_markup=config.menu())
     else:
         del config.arr[-1]
-        bot.send_message(message.chat.id, 'Ваши последние изменения были удалены 🗑', reply_markup=config.menu())
-
-
-#  Удаление
-def del_index(message):
-    bot.send_message(message.chat.id, config.LIST(config.arr))
-    Del = message.text
-    Del = Del.split(' ')
-    arr_test = config.arr
-
-    for i in range(len(Del) - 1):
-        if int(Del[i]) < len(config.arr):
-            del arr_test[int(Del[i]) - 1]
-    config.arr = arr_test
-    bot.send_message(message.chat.id, arr_test, reply_markup=config.menu())
+        bot.send_message(message.chat.id, 'Ваши последние изменения были удалены', reply_markup=config.menu())
 
 
 @bot.message_handler(content_types=['text'])
@@ -77,20 +48,15 @@ def bot_list(message):
 
         elif message.text == "Добавить 📌":
             add = bot.send_message(message.chat.id, 'Введите текст:')
-            bot.register_next_step_handler(add, add_f)
+            bot.register_next_step_handler(add, addF)
 
         elif message.text == "Нет, спасибо ❌":
             del config.arr[-1]
-            bot.send_message(message.chat.id, 'Ваши последние изменения не были добавлены 🗑', reply_markup=config.menu())
+            bot.send_message(message.chat.id, 'Ваши последние изменения не были добавлены', reply_markup=config.menu())
 
         elif message.text == "Добавить ⏰":
             time = bot.send_message(message.chat.id, 'Введите время:')
             bot.register_next_step_handler(time, time_add)
-
-        elif message.text == "Удалить 🗑":
-            bot.send_message(message.chat.id, config.LIST(config.arr))
-            Del = bot.send_message(message.chat.id, 'Введите что вы хотите удалить:')
-            bot.register_next_step_handler(Del, del_index)
 
 
 bot.polling(none_stop=True)
